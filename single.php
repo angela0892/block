87b930f608ee6945da3699b60122f2e16f8e1ae4
angelanatividad108@gmail.com <angelanatividad108@gmail.com>
2022-05-11 22:02:17 -0500

    fuente

--- single.php
+++ single.php
@@ -3,12 +3,18 @@
 
 		<section >
 			<center>
+				<h2><?php the_title();?></h2>
+				<H5> Resumen:<?php the_field('resumen'); ?> </H5>
+
+
 				<?php the_post_thumbnail("large");?>
 
-				<h2><?php the_title();?></h2>
+			
 
-				<?php the_content();?>
+			<?php the_content();?>
 
+				<H6> fuente: <?php the_field('fuente'); ?></H6>
+				
 			</center>
 
 			</section>
